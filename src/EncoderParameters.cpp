6eec34d44b5e835775e2aab39f18b621bc222b14
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-09-02 16:44:40 -0300

    Fix: encoder parameters

--- src/EncoderParameters.cpp
+++ src/EncoderParameters.cpp
@@ -1,7 +1,13 @@
 
 #include "EncoderParameters.h"
+EncoderParameters EncoderParameters::parameters;
+
+void EncoderParameters::set_global_parameters() const {
+  EncoderParameters::parameters = *this;
+}
 
 void EncoderParameters::parse_cli(int argc, char *argv[]) {
+    this->initialized = true;
     for (int it = 1; it < argc; ++it) {
         std::string flag = argv[it];
 
@@ -30,17 +36,13 @@ void EncoderParameters::parse_cli(int argc, char *argv[]) {
         } else if (flag == "-lfv") {
             this->dim_LF.v = (short)strtol(argv[++it], nullptr, 10);
         } else if (flag == "-lytro") {
-            this->lytro = true; //(short)strtol(argv[++it], nullptr, 10);
+            this->lytro = true;
         } else if (flag == "-input") {
             this->path_input = argv[++it];
         } else if (flag == "-output") {
             this->path_output = argv[++it];
         } else if (flag == "-qp") {
             this->qp = strtof(argv[++it], nullptr);
-        } else if (flag == "-transform") {
-            this->transform = argv[++it];
-        } else if (flag == "-flipaxis") {
-            this->flipaxis = strtol(argv[++it], nullptr, 10);
         } else if (flag == "-lee-c") {
             this->lee_c = strtof(argv[++it], nullptr);
         } else if (flag == "-lee-a0") {
@@ -51,6 +53,20 @@ void EncoderParameters::parse_cli(int argc, char *argv[]) {
             this->lee_b0 = strtof(argv[++it], nullptr);
         } else if (flag == "-lee-bi") {
             this->lee_bi = strtof(argv[++it], nullptr);
+        } else if (flag == "-lambda") {
+            this->lambda = strtof(argv[++it], nullptr);
+        } else if (flag == "-show-progress-bar") {
+            this->show_progress_bar = true;
+        } else if (flag == "-verbose") {   
+            this->verbose = true;
+        } else if (flag == "-experimental"){
+            this->experimental = true;
+        } else if (flag == "-transform"){
+            this->transform = argv[++it];
+        } else if (flag == "-enforce-dct-for-non-luma") {
+            this->dct_for_non_luma = true;
+        } else if (flag == "-max-splits") {
+            this->max_splits = (int)strtol(argv[++it], nullptr, 10);
         } else {
             std::cout << "Unused Option: " << argv[it];
             std::cout << "\t" << argv[++it] << std::endl;
@@ -59,6 +75,7 @@ void EncoderParameters::parse_cli(int argc, char *argv[]) {
 
     this->dim_LF.updateNSamples();
     this->dim_block.updateNSamples();
+    set_global_parameters();
 }
 
 float EncoderParameters::getQp() const { return this->qp; }
@@ -68,21 +85,19 @@ const std::string &EncoderParameters::getPathInput() const { return this->path_i
 const std::string &EncoderParameters::getPathOutput() const { return this->path_output; }
 
 void EncoderParameters::report() {
-    std::cout << "################ Encoder Parameters ################" << std::endl;
-    std::cout << "Input path: " << this->path_input << std::endl;
-    std::cout << "Output path: " << this->path_output << std::endl;
-    std::cout << std::endl;
-    std::cout << "LF Dim: " << this->dim_LF << std::endl;
-    std::cout << "Block Dim: " << this->dim_block << std::endl;
-    std::cout << "Lytro: " << this->lytro << std::endl;
-    std::cout << std::endl;
-    std::cout << "Transform: " << this->transform << std::endl;
-    std::cout << "Segments: " << this->segments << std::endl;
-    std::cout << "Flip Axis: " << this->flipaxis << std::endl;
-    std::cout << "QP: " << this->qp << std::endl;
-    std::cout << "Quantization Weight (*100): " << this->quant_weight_100 << std::endl;
-    std::cout << "############################################" << std::endl;
-    std::cout << std::endl;
+    std::cout <<"[Report]\n";
+    display_report(std::cout, "Input path", this->path_input) ;
+    display_report(std::cout, "Output path", this->path_output) ;
+    display_report(std::cout, "LF Dim", this->dim_LF) ;
+    display_report(std::cout, "Block Dim", this->dim_block) ;
+    display_report(std::cout, "Transform", this->transform) ;
+    display_report(std::cout, "QP", this->qp) ;
+    display_report(std::cout, "Quantization Weight (*100)", this->quant_weight_100) ;
+    display_report(std::cout, "Lytro", (this->lytro ? "YES" : "NO")) ;
+    display_report(std::cout, "Lambda", this->lambda) ;
+    display_report(std::cout, "Max Split", this->max_splits) ;
+    display_report(std::cout, "Experimental Features", (this->experimental ? "YES" : "NO"))  ;
+    std::cout.flush();
 }
 
 bool EncoderParameters::isLytro() const { return this->lytro; }
--- src/EncoderParameters.h
+++ src/EncoderParameters.h
@@ -5,25 +5,44 @@
 #include <string>
 
 #include "Point4D.h"
+#include "utils.h"
 
 class EncoderParameters {
-  float qp{1.0};
+
   std::string path_input{"input/"}, path_output{"output/"};
 
+  void set_global_parameters() const;
+
 public:
+    bool initialized = false;
+    static EncoderParameters parameters;
+    float qp{1.0};
     Point4D dim_LF{0, 0, 0, 0},
             dim_block{0, 0, 0, 0},
             quant_weight_100{1 * 100, 1 * 100, 1 * 100, 1 * 100};
-    bool lytro{false};
+    bool lytro{true};
     std::string transform;
-    int flipaxis {0};
-    int segments {0};
+    float lambda = 1;
     float lee_c = 10;
     float lee_ai = 1023;
     float lee_a0 = 1023;
     float lee_bi = 0.04;
     float lee_b0 = 0.01;
-    
+    bool show_progress_bar = false;
+    bool verbose = false;
+    bool experimental = true;
+    bool dct_for_non_luma = false;
+    int max_splits = 0;
+    bool calculate_metrics = true;
+    bool display_stages = true;
+    bool calculate_transform_usage = true;
+    bool sort_coefficients = true;
+    enum {
+      ZIG_ZAG_SCAN_ORDER,
+      CUSTOM_SCAN_ORDER,
+      Z_ORDER_CURVE,
+    };
+    int scan_order = CUSTOM_SCAN_ORDER;
     bool isLytro() const;
 
   EncoderParameters() = default;
@@ -38,5 +57,7 @@ public:
 
   void report();
 };
+
+
 
 #endif // ENCODERPARAMETERS_H
