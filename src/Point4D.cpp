b0772d180119fd80718eaca17c6fe7798eae7f16
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-09-02 16:00:52 -0300

    Feature: Point4D uses value_type

--- src/Point4D.cpp
+++ src/Point4D.cpp
@@ -1,35 +1,40 @@
 
 #include "Point4D.h"
+#include "sstream"
 
-Point4D::Point4D(uint x, uint y, uint u, uint v) : x(x), y(y), u(u), v(v) {
+Point4D::Point4D(Point4D::value_type x, Point4D::value_type y, Point4D::value_type u, Point4D::value_type v) : x(x), y(y), u(u), v(v)
+{
   this->updateNSamples();
 }
 
+Point4D::value_type Point4D::getNSamples() const { return this->nSamples; }
 
-
-uint Point4D::getNSamples() const { return this->nSamples; }
-
-void Point4D::updateNSamples() {
+void Point4D::updateNSamples()
+{
   this->nSamples = this->x * this->y * this->u * this->v;
 }
 
-std::ostream &operator<<(std::ostream &os, Point4D const &point) {
-  os << "{"
-     << "x: " << std::setfill('0') << std::setw(4) << point.x << ", "
-     << "y: " << std::setfill('0') << std::setw(4) << point.y << ", "
-     << "u: " << std::setfill('0') << std::setw(4) << point.u << ", "
-     << "v: " << std::setfill('0') << std::setw(4) << point.v << "}";
-
+std::ostream &operator<<(std::ostream &os, Point4D const &point)
+{
+  std::stringstream ss;
+  ss << "("
+     << point.x << ", "
+     << point.y << ", "
+     << point.u << ", "
+     << point.v << ")";
+  os << ss.str();
   return os;
 }
 
-bool operator==(const Point4D &p1, const Point4D &p2) {
+bool operator==(const Point4D &p1, const Point4D &p2)
+{
   return (p1.x == p2.x && p1.y == p2.y && p1.u == p2.u && p1.v == p2.v);
 }
 
 bool operator!=(const Point4D &p1, const Point4D &p2) { return !(p1 == p2); }
 
-Point4D operator-(const Point4D &p1, const Point4D &p2) {
+Point4D operator-(const Point4D &p1, const Point4D &p2)
+{
   if (p2.x > p1.x || p2.y > p1.y || p2.u > p1.u || p2.v > p1.v)
     throw std::overflow_error("Tried to subtract a bigger value from "
                               "smaller one.");
@@ -38,17 +43,92 @@ Point4D operator-(const Point4D &p1, const Point4D &p2) {
   return p;
 }
 
-Point4D operator+(const Point4D &p1, const Point4D &p2) {
+Point4D operator+(const Point4D &p1, const Point4D &p2)
+{
   Point4D p(p1.x + p2.x, p1.y + p2.y, p1.u + p2.u, p1.v + p2.v);
   return p;
 }
 
-Point4D operator+(const Point4D &p, const uint delta) {
+Point4D operator+(const Point4D &p, const Point4D::value_type delta)
+{
   Point4D q(delta, delta, delta, delta);
   return p + q;
 }
 
-Point4D operator-(const Point4D &p, const uint delta) {
+Point4D operator-(const Point4D &p, const Point4D::value_type delta)
+{
   Point4D q(delta, delta, delta, delta);
   return p - q;
+}
+
+void Point4D::set(Point4D::value_type x, Point4D::value_type y, Point4D::value_type u, Point4D::value_type v)
+{
+  this->x = x;
+  this->y = y;
+  this->u = u;
+  this->v = v;
+  updateNSamples();
+}
+
+std::vector<Point4D::value_type> Point4D::to_vector() const
+{
+  std::vector<Point4D::value_type> vec;
+  vec.resize(4);
+  vec[0] = x;
+  vec[1] = y;
+  vec[2] = u;
+  vec[3] = v;
+  return vec;
+}
+
+Point4D operator>>(const Point4D &p, const int val)
+{
+  Point4D q;
+  q.x = p.x >> val;
+  q.y = p.y >> val;
+  q.u = p.u >> val;
+  q.v = p.v >> val;
+  return q;
+}
+
+const Point4D::value_type &Point4D::operator[](size_t dimension) const
+{
+  switch (dimension)
+  {
+  case 0:
+    return x;
+  case 1:
+    return y;
+  case 2:
+    return u;
+  case 3:
+    return v;
+  }
+  throw;
+}
+
+Point4D::value_type &Point4D::operator[](size_t dimension)
+{
+  switch (dimension)
+  {
+  case 0:
+    return x;
+  case 1:
+    return y;
+  case 2:
+    return u;
+  case 3:
+    return v;
+  }
+  throw;
+}
+
+bool operator<(const Point4D &p, const Point4D &q)
+{
+  return p.x < q.x && p.y < q.y && p.u < q.u && p.v < q.v;
+}
+
+bool operator>(const Point4D &p, const Point4D &q)
+{
+  return p.x > q.x && p.y > q.y && p.u > q.u && p.v > q.v;
 }
--- src/Point4D.h
+++ src/Point4D.h
@@ -5,32 +5,26 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
-#include <zconf.h>
 
-class Point4D {
+class Point4D
+{
 public:
-  uint x{8}, y{8}, u{8}, v{8};
-  uint nSamples{0};
+  using value_type = std::size_t;
+  value_type x{8}, y{8}, u{8}, v{8};
+  value_type nSamples{0};
 
   Point4D() = default;
 
-  Point4D(uint x, uint y, uint u, uint v);
+  Point4D(value_type x, value_type y, value_type u, value_type v);
   template <typename T>
-  Point4D(const T *array): x(array[0]), y(array[1]), u(array[2]), v(array[3]) {updateNSamples();}
+  Point4D(const T *array);
 
   void updateNSamples();
+  value_type getNSamples() const;
 
-  uint getNSamples() const;
+  void set(value_type x, value_type y, value_type u, value_type v);
 
-  std::vector<uint> to_vector() const {
-    std::vector<uint> vec;
-    vec.resize(4);
-    vec[0] = x;
-    vec[1] = y;
-    vec[2] = u;
-    vec[3] = v;
-    return vec;
-  }
+  std::vector<value_type> to_vector() const;
 
   friend std::ostream &operator<<(std::ostream &os, Point4D const &point);
 
@@ -38,41 +32,30 @@ public:
 
   friend bool operator==(const Point4D &p1, const Point4D &p2);
 
-  friend Point4D operator>>(const Point4D &p, const int val) {
-    Point4D q;
-    q.x = p.x >> val;
-    q.y = p.y >> val;
-    q.u = p.u >> val;
-    q.v = p.v >> val;
-    return q;
-  }
+  friend Point4D operator>>(const Point4D &p, const int val);
 
-  uint &operator[](size_t dimension) {
-    switch (dimension) {
-    case 0:
-      return x;
-    case 1:
-      return y;
-    case 2:
-      return u;
-    case 3:
-      return v;
-    }
-    throw;
-  }
+  const value_type &operator[](size_t dimension) const;
 
-  friend bool operator<(const Point4D& p, const Point4D& q) {
-    return p.x < q.x && p.y < q.y && p.u < q.u && p.v < q.v;
-  }
+  value_type &operator[](size_t dimension);
+
+  friend Point4D operator+(const Point4D &p, const Point4D &q);
+
+  friend bool operator<(const Point4D &p, const Point4D &q);
+
+  friend bool operator>(const Point4D &p, const Point4D &q);
+
+  friend Point4D operator-(const Point4D &p, const Point4D &q);
 };
-
 
 struct Point4DHasher
 {
-  std::size_t operator () (const Point4D &p) const 
+  Point4D::value_type operator()(const Point4D &p) const
   {
     return p.x ^ ((p.y << 1) ^ ((p.u << 2) ^ (p.v << 3)));
   }
 };
+
+template <typename T>
+Point4D::Point4D(const T *array) : x(array[0]), y(array[1]), u(array[2]), v(array[3]) { updateNSamples(); }
 
 #endif // POINT4D_H
