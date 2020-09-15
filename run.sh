7977e334a5da60aa23027bc82f8067a0cfbc7a1e
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-09-15 15:43:25 -0300

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # Default values
-BINARY="./cmake-build-release/LF_Codec"
-DATASET_DIR="../LFCodecResources/Datasets"
-RESULT_DIR="./results"
+if [ "$BINARY" != "" ]; then
+  echo "Variable BINARY not found. Using default value."
+  BINARY="./cmake-build-release/LF_Codec"
+fi
+if [ "$DATASET_DIR" != "" ]; then
+  echo "Variable DATASET_DIR not found. Using default value."
+  DATASET_DIR="../LFCodecResources/Datasets"
+fi
+if [ "RESULT_DIR" != "" ]; then
+  echo "Variable RESULT_DIR not found. Using default value."
+  RESULT_DIR="./results"
+fi
 
 QP=1
 QX=1
